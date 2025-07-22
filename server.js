d66e4cfd7d9e6408590deab301d6d8a6df915cee
BUsuper <merzlyakov96@gmail.com>
2025-07-22 20:49:02 +1000

    Update server to implement the API

--- server.js
+++ server.js
@@ -1,24 +1,58 @@
 import http from "http";
 import { sendResponse } from "./utils/sendResponse.js";
+import crosswords from "./crosswords.ts";
 
 const port = 8001;
 
 const server = http.createServer((req, res) => {
   const urlObject = new URL(req.url, `http://${req.headers.host}`);
+  const crosswordsKeys = Object.keys(crosswords);
 
   if (urlObject.pathname === "/api") {
+    const searchParams = Object.fromEntries(urlObject.searchParams);
+    const isSearchParamsCorrectLength = Object.keys(searchParams).length === 1;
+    if (isSearchParamsCorrectLength) {
+      const { date } = searchParams;
+      if (!date) {
+        sendResponse(
+          res,
+          200,
+          "text/plain",
+          "Use queries in the form of /api?date=1.1.2025 or /api/keys"
+        );
+      }
+
+      if (crosswordsKeys.includes(date)) {
+        sendResponse(
+          res,
+          200,
+          "application/json",
+          JSON.stringify(crosswords[date])
+        );
+      } else {
+        sendResponse(
+          res,
+          404,
+          "text/plain",
+          `Crossword with date ${date} not found`
+        );
+      }
+    } else {
+      sendResponse(
+        res,
+        200,
+        "text/plain",
+        "Use queries in the form of /api?date=1.1.2025"
+      );
+    }
+  } else if (urlObject.pathname === "/api/keys") {
+    sendResponse(res, 200, "application/json", JSON.stringify(crosswordsKeys));
+  } else {
     sendResponse(
       res,
       200,
       "text/plain",
-      "Hello there, you're using the API correctly"
-    );
-  } else {
-    sendResponse(
-      res,
-      404,
-      "text/plain",
-      "Error you're not using the API correctly"
+      "Use queries in the form of /api?date=1.1.2025 or /api/keys"
     );
   }
 });
